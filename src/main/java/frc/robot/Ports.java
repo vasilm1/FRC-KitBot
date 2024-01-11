39d47fed59a50ea94fcdda7bc6d040b5f5562444
Vasil <58887500+Vasil789@users.noreply.github.com>
2024-01-11 09:34:40 -0600

    Update Ports.java

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class Ports {
+    public static final int DRIVE_RIGHT_2 = 1;
+    public static final int DRIVE_RIGHT_1 = 2;
+    public static final int DRIVE_LEFT_1 = 3;
+    public static final int DRIVE_LEFT_2 = 4;
+    public static final int LAUNCHER = 5;
+}
