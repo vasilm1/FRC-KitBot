b582356fc1c2c2b397eef8dbf9c92f344539cf65
Vasil <58887500+Vasil789@users.noreply.github.com>
2024-01-11 09:37:14 -0600

    Update Drivetrain.java

--- src/main/java/frc/robot/subsys/Drivetrain.java
+++ src/main/java/frc/robot/subsys/Drivetrain.java
@@ -1,24 +1,26 @@
 package frc.robot.subsys;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import frc.robot.Misc;
 import frc.robot.Ports;
 
+
+
 public class Drivetrain {
-
-    private DriveSpeed driveSpeed = DriveSpeed.FAST;
-
+    
+    private DriveSpeed DSpeed = DriveSpeed.FAST;
+    
     private static CANSparkMax rightFront;
     private static CANSparkMax rightBack;
     private static CANSparkMax leftFront;
     private static CANSparkMax leftBack;
 
     private static Drivetrain instance;
-
+    
     public enum DriveSpeed {
         SLOW(0.1),
         FAST(0.25);
@@ -30,43 +32,38 @@ public class Drivetrain {
         }
     }
 
-    public Drivetrain() {
-        /* Right motor group */
 
-        rightFront = new CANSparkMax(Ports.DRIVE_RIGHT_1, MotorType.kBrushed);
-        rightFront.setInverted(true);
-        rightFront.setOpenLoopRampRate(0.5);
-        rightFront.setClosedLoopRampRate(0.5);
-        rightFront.setIdleMode(IdleMode.kBrake);
-        rightFront.burnFlash();
+    public Drivetrain(){
+    rightFront = new CANSparkMax(Ports.DRIVE_RIGHT_1, MotorType.kBrushed);
+    rightFront.setInverted(true);
+    rightFront.setOpenLoopRampRate(0.5);
+    rightFront.setClosedLoopRampRate(0.5);
+    rightFront.setIdleMode(IdleMode.kBrake);
+    rightFront.burnFlash();
 
-        rightBack = new CANSparkMax(Ports.DRIVE_RIGHT_2, MotorType.kBrushed);
-        rightBack.setInverted(true);
-        rightBack.setOpenLoopRampRate(0.5);
-        rightBack.setClosedLoopRampRate(0.5);
-        rightBack.setIdleMode(IdleMode.kBrake);
-        rightBack.burnFlash();
+    rightBack = new CANSparkMax(Ports.DRIVE_RIGHT_2, MotorType.kBrushed);
+    rightBack.setInverted(true);
+    rightBack.setOpenLoopRampRate(0.5);
+    rightBack.setClosedLoopRampRate(0.5);
+    rightBack.setIdleMode(IdleMode.kBrake);
+    rightBack.burnFlash();
 
-        /* Left motor group */
-        leftFront = new CANSparkMax(Ports.DRIVE_LEFT_1, MotorType.kBrushed);
-        leftFront.setInverted(false);
-        leftFront.setOpenLoopRampRate(0.5);
-        leftFront.setClosedLoopRampRate(0.5);
-        leftFront.setIdleMode(IdleMode.kBrake);
-        leftFront.burnFlash();
+    leftFront = new CANSparkMax(Ports.DRIVE_LEFT_1, MotorType.kBrushed);
+    leftFront.setOpenLoopRampRate(0.5);
+    leftFront.setClosedLoopRampRate(0.5);
+    leftFront.setIdleMode(IdleMode.kBrake);
+    leftFront.burnFlash();
 
-        leftBack = new CANSparkMax(Ports.DRIVE_LEFT_2, MotorType.kBrushed);
-        leftBack.setInverted(false);
-        leftBack.setOpenLoopRampRate(0.5);
-        leftBack.setClosedLoopRampRate(0.5);
-        leftBack.setIdleMode(IdleMode.kBrake);
-        leftBack.burnFlash();
-
+    leftBack = new CANSparkMax(Ports.DRIVE_LEFT_2, MotorType.kBrushed);
+    leftBack.setOpenLoopRampRate(0.5);
+    leftBack.setClosedLoopRampRate(0.5);
+    leftBack.setIdleMode(IdleMode.kBrake);
+    leftBack.burnFlash();
     }
 
     public void drive(double forward, double turn) {
-        double leftSpeed = Misc.clamp((forward * driveSpeed.speed) - turn, -1, 1);
-        double rightSpeed = Misc.clamp((forward * driveSpeed.speed) + turn, -1, 1);
+        double leftSpeed = Misc.clamp((forward * DSpeed.speed) - turn, -1, 1);
+        double rightSpeed = Misc.clamp((forward * DSpeed.speed) + turn, -1, 1);
 
         SmartDashboard.putNumber("RIGHT SPEED", (int) (rightSpeed * 100));
         SmartDashboard.putNumber("LEFT SPEED", (int) (leftSpeed * 100));
@@ -83,7 +80,7 @@ public class Drivetrain {
     }
 
     public void setDriveSpeed(DriveSpeed driveSpeed) {
-        this.driveSpeed = driveSpeed;
+        this.DSpeed = driveSpeed;
     }
 
     public static Drivetrain getInstance() {
