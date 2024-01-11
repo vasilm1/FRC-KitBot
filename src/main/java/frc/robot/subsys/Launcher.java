4a53907122c55f06106724266411fb0bfb94bfdb
Vasil <58887500+Vasil789@users.noreply.github.com>
2024-01-11 09:49:12 -0600

    Update Launcher.java

--- src/main/java/frc/robot/subsys/Launcher.java
+++ src/main/java/frc/robot/subsys/Launcher.java
@@ -4,7 +4,6 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import frc.robot.Misc;
 import frc.robot.Ports;
 
 public class Launcher {
@@ -18,6 +17,7 @@ public class Launcher {
     {
         SHOOT(1.0),
         OFF(0.0),
+        INTAKE(-1.0);
 
         public final double power;
 
@@ -35,6 +35,28 @@ public class Launcher {
         launcher.setClosedLoopRampRate(0);
         launcher.setIdleMode(IdleMode.kBrake);
         launcher.burnFlash();
+    }
+
+    public void setState(LauncherState state) {
+        this.state = state;
+    }
+
+    public void updateState(boolean launchbutton, boolean intakebutton)
+    {
+        if(launchbutton && !intakebutton)
+        {
+            setState(LauncherState.SHOOT);
+        }
+        if(intakebutton && !launchbutton)
+        {
+            setState(LauncherState.INTAKE);
+        }
+        else setState(LauncherState.OFF);
+
+        launcher.set(state.power);
+
+        SmartDashboard.putNumber("LAUNCHER VOLTS", launcher.getBusVoltage());
+
     }
 
     public static Launcher getInstance() {
