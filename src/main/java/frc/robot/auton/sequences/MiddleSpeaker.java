0540ecf056bf705bac0dca1653194c45b0581fb6
Vasil <58887500+vasilm1@users.noreply.github.com>
2024-03-18 10:25:51 -0500

    auton

---
+++ src/main/java/frc/robot/auton/sequences/MiddleSpeaker.java
@@ -0,0 +1,11 @@
+package frc.robot.auton.sequences;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.auton.commands.*;
+
+public class RFarToSpeaker extends SequentialCommandGroup {
+    
+    public RFarToSpeaker(){
+
+    }
+}
